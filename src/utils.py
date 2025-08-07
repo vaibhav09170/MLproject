26f626d47144cbd56c2bd982419dfb634b307c29
vaibhav09170 <vaibhav09170@gmail.com>
2025-08-07 14:33:56 +1000

    Predict Pipeline with web flask web app created

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+import pickle
 
 from src.exception import Custom_Exception
 from src.logger import logging
@@ -50,3 +51,12 @@ def evaluate_models(X_train, y_train, X_test, y_test,models,param):
             
     except Exception as e:
         raise Custom_Exception(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+        logging.info("Object has been loaded")
+        
+    except Exception as e:
+        raise Custom_Exception(e, sys)
